40ecd72a49651d2952d661d7c040979ada5a674d
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-02-22 16:49:11 +0200

    updaate view

--- retargeting_feed_generator/routes.py
+++ retargeting_feed_generator/routes.py
@@ -5,5 +5,5 @@ __author__ = 'kuzmenko-pavel'
 def includeme(config):
     config.add_route('index', '/')
     config.add_route('feeds', '/feeds')
-    config.add_route('export', '/export/{id:[0-9a-fA-F\-]{36}}.xml')
+    config.add_route('export', '/export/{id}.xml')
     config.add_static_view('static', 'static', cache_max_age=0)
--- retargeting_feed_generator/views.py
+++ retargeting_feed_generator/views.py
@@ -1,14 +1,11 @@
 import os
-from pyramid.view import view_config
-from pyramid.view import forbidden_view_config
-from pyramid.response import FileResponse
+
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.httpexceptions import HTTPUnauthorized
+from pyramid.response import FileResponse
 from pyramid.security import forget
-from collections import defaultdict
-
-from retargeting_feed_generator.tasks import create_feed
-from retargeting_feed_generator.helper import redirect_link, image_link, price
+from pyramid.view import forbidden_view_config
+from pyramid.view import view_config
 
 
 @view_config(route_name='index', renderer='templates/index.html', permission='view')
@@ -20,7 +17,7 @@ def index(request):
 @view_config(route_name='export', renderer='templates/xml.html', permission='view')
 def export(request):
     request.response.content_type = 'application/xml'
-    id = request.matchdict.get('id', '').upper()
+    id = request.matchdict.get('id', '')
     dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/xml')
     file_path = os.path.join(dir_path, id + ".xml")
     file_exists = os.path.isfile(file_path)
@@ -38,29 +35,17 @@ def export(request):
 @view_config(route_name='feeds', renderer='json', permission='view')
 def feeds(request):
     data = []
-    q = [
-        'm.Name IS NOT NULL',
-        'isActive=1'
-    ]
-    sql = '''
-                            SELECT
-                            a.AdvertiseID AS AdvertiseID,
-                            Title,
-                            Login, 
-                            m.Name AS Manager,
-                            CASE WHEN MarketID IS NULL THEN 'false' ELSE 'true' END Market
-                            FROM View_Advertise a
-                            LEFT OUTER JOIN Users u ON u.UserID = a.UserID
-                            LEFT OUTER JOIN Manager m  ON u.ManagerID = m.id
-                            LEFT OUTER JOIN MarketByAdvertise mark ON mark.AdvertiseID = a.AdvertiseID
-                            WHERE %s
-                            ''' % (' AND '.join(q))
-    result = request.dbsession.execute(sql)
-    for campaign in result:
-        data.append((campaign[1], campaign[2],
-                     "<a href='%s' target='_blank''>File Export</a>" % request.route_url('export',
-                                                                                         id=campaign[0])
-                     ))
+    dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/xml')
+    files = os.listdir(dir_path)
+    for name in files:
+        if '.xml' in name:
+            name = name.replace('.xml', '')
+            tmp = name.split('::')
+            if len(tmp) == 2:
+                data.append((tmp[1],
+                             tmp[0],
+                             "<a href='%s' target='_blank''>File Export</a>" % request.route_url('export', id=name)
+                             ))
     return {
         'data': data
     }
