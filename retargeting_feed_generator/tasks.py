7d3176ddc39a5e85a94abac518f1855c77e26516
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-03-06 10:32:12 +0200

    update create feed

--- retargeting_feed_generator/tasks.py
+++ retargeting_feed_generator/tasks.py
@@ -61,6 +61,7 @@ def check_feed():
     result = dbsession.execute('''
         SELECT M.UserID
           ,M.MarketID
+          ,M.Title
           ,U.Login
       FROM Market as M
       INNER JOIN Users as U ON U.UserID = M.UserID
@@ -69,8 +70,9 @@ def check_feed():
     for adv in result:
         user_id = adv[0]
         market_id = adv[1]
-        login = re.sub('[^0-9a-zA-Z]+', '_', adv[2])
-        data[(user_id, login)].append("'%s'" % market_id)
+        title = re.sub('[^0-9a-zA-Z]+', '_', adv[2])
+        login = re.sub('[^0-9a-zA-Z]+', '_', adv[3])
+        data[(user_id, login)].append((("'%s'" % market_id), title))
     result.close()
     dbsession.commit()
     for key, value in data.items():
@@ -79,10 +81,10 @@ def check_feed():
 
 
 @app.task(ignore_result=True)
-def create_feed(user_id, login, market_ids):
+def create_feed(user_id, login, markets):
     print('START CREATE FEED %s' % user_id)
-    line = 0
     ids = []
+    data = defaultdict(lambda:{'markets': [], 'offers': []})
     r = redis.Redis(host='srv-13.yottos.com', port=6379, db=10)
     exists = r.exists('exists::%s' % user_id)
     if exists:
@@ -97,21 +99,19 @@ def create_feed(user_id, login, market_ids):
                     c = 0
                 if c > 0:
                     ids.append((of_id, c))
+            if len(ids) > 100:
+                break
 
     ids.sort(key=lambda x: x[1], reverse=True)
     if ids:
-        dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/xml')
-        file_path = os.path.join(dir_path, "%s::%s.xml" % (login, user_id))
-        temp_file = file_path + '.' + str(int(time.time()))
-        with open(temp_file, 'w', encoding='utf-8', errors='xmlcharrefreplace') as f:
-            data = {
-                'login': login,
-                'date': datetime.now().strftime('%Y-%m-%d %H:%M')
-            }
-            f.write(tpl_xml_start.format(**data))
-            f.flush()
+        for market_id, title in markets:
+            data['ALL']['markets'].append(market_id.upper())
+            data[title]['markets'].append(market_id.upper())
+        if len(data['ALL']['markets']) > 0:
             for item in ids:
                 dbsession = app.conf['PYRAMID_REGISTRY']['dbsession_factory']()
+                market_ids = data['ALL']['markets']
+                market_by_id = {str(x).upper(): str(y) for x, y in markets}
                 result = dbsession.execute('''
                 SELECT TOP 1 [Title]
                             ,[Descript]
@@ -120,12 +120,13 @@ def create_feed(user_id, login, market_ids):
                             ,[ImgURL]
                             ,[Logo]
                             ,[Recommended]
+                            ,[MarketID]
                   FROM Lot
                   WHERE MarketID in (%s)
                   and RetargetingID= '%s'
                 ''' % (','.join(market_ids), item[0]))
                 for offer in result:
-                    data = {
+                    lot = {
                         'offer_id': item[0],
                         'name': text_normalize(offer[0]),
                         'description': text_normalize(offer[1]),
@@ -136,17 +137,34 @@ def create_feed(user_id, login, market_ids):
                         'recommended': offer[6],
                         'sale_count': item[1]
                     }
-                    f.write(tpl_xml_offer.format(**data))
-                    line += 1
+                    data['ALL']['offers'].append(lot)
+                    market_title = market_by_id.get(str("'%s'" % offer[7]).upper())
+                    if market_title:
+                        data[market_title]['offers'].append(lot)
                 result.close()
                 dbsession.commit()
+    for k, v in data.items():
+        line = 0
+        dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/xml')
+        file_path = os.path.join(dir_path, "%s::%s::%s.xml" % (login, user_id, k))
+        temp_file = file_path + '.' + str(int(time.time()))
+        with open(temp_file, 'w', encoding='utf-8', errors='xmlcharrefreplace') as f:
+            template_data = {
+                'login': login,
+                'date': datetime.now().strftime('%Y-%m-%d %H:%M')
+            }
+            f.write(tpl_xml_start.format(**template_data))
+            f.flush()
+            for item in v.get('offers', []):
+                f.write(tpl_xml_offer.format(**item))
+                line += 1
                 if line > 100:
                     f.flush()
                     break
-            f.flush()
+                f.flush()
             f.write(tpl_xml_end)
             f.flush()
         move(temp_file, file_path)
     r.connection_pool.disconnect()
     del ids
-    print('STOP CREATE FEED %s on %d offers' % (user_id, line))
+    print('STOP CREATE FEED %s' % user_id)
